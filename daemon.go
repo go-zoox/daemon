e64c7ac464cb7882c2ab9a132b79800780a3d14c
whatwerwant <tobewhatwewant@outlook.com>
2023-01-30 14:38:04 +0800

    chore: add pid file

--- daemon.go
+++ daemon.go
@@ -5,14 +5,28 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/go-zoox/fs"
 	"github.com/go-zoox/logger"
+	"github.com/go-zoox/logger/components/transport"
+	"github.com/go-zoox/logger/transport/file"
 )
 
 type Config struct {
-	BackgroundConfig
+	Cmd     string
+	Args    []string
+	Role    string
+	LogFile string
+	PidFile string
 }
 
 func Daemon(cfg *Config) error {
+	logger.SetTransports(map[string]transport.Transport{
+		"file": file.New(&file.Config{
+			Level: "info",
+			File:  cfg.LogFile,
+		}),
+	})
+
 	cmd, err := Background(&BackgroundConfig{
 		Role:    RoleMaster,
 		LogFile: cfg.LogFile,
@@ -28,21 +42,45 @@ func Daemon(cfg *Config) error {
 		return nil
 	}
 
-	logger.Infof("[daemon: %d] start ...", os.Getegid())
+	if cfg.PidFile != "" {
+		// @TODO
+		if fs.IsExist(cfg.PidFile) {
+			if err := fs.RemoveFile(cfg.PidFile); err != nil {
+				return fmt.Errorf("[pid: %d] failed to remove pid file(%s): %v", os.Getpid(), cfg.PidFile, err)
+			}
+		}
+
+		if err := fs.WriteFile(cfg.PidFile, []byte(fmt.Sprintf("%d", os.Getpid()))); err != nil {
+			return fmt.Errorf("[pid: %d] failed to write pid to file(%s): %v", os.Getpid(), cfg.PidFile, err)
+		}
+	}
+
+	logger.Infof("[daemon: %d] start ...", os.Getpid())
 
 	realCmd := &exec.Cmd{
 		Path: cfg.Cmd,
 		Args: append([]string{cfg.Cmd}, cfg.Args...),
 		Env:  os.Environ(),
 	}
+
+	if cfg.LogFile != "" {
+		stdout, err := os.OpenFile(cfg.LogFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			return fmt.Errorf("[pid: %d] failed to open log file: %v", os.Getpid(), err)
+		}
+
+		realCmd.Stderr = stdout
+		realCmd.Stdout = stdout
+	}
+
 	if err := realCmd.Start(); err != nil {
-		return fmt.Errorf("[daemon: %d] failed to start daemon: %v", os.Getegid(), err)
+		return fmt.Errorf("[daemon: %d] failed to start daemon: %v", os.Getpid(), err)
 	}
 
 	if err := realCmd.Wait(); err != nil {
-		return fmt.Errorf("[daemon: %d] failed to wait daemon: %v", os.Getegid(), err)
+		return fmt.Errorf("[daemon: %d] failed to wait daemon: %v", os.Getpid(), err)
 	}
 
-	logger.Infof("[daemon: %d] exit", os.Getegid())
+	logger.Infof("[daemon: %d] exit", os.Getpid())
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/go-zoox/daemon
 
 go 1.19
 
-require github.com/go-zoox/logger v1.3.4
+require (
+	github.com/go-zoox/fs v1.3.3
+	github.com/go-zoox/logger v1.3.4
+)
 
 require (
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-zoox/chalk v1.0.1 // indirect
 	github.com/go-zoox/datetime v1.1.0 // indirect
+	github.com/go-zoox/uuid v0.0.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 )
